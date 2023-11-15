3cb1392d64f908878b31339577fca30ef0482220
BBren0 <128422514+BBren0@users.noreply.github.com>
2023-11-15 15:03:37 -0300

    Commit 1

---
+++ api_cp/src/main/java/br/com/cp/api_cp/ApiCpApplication.java
@@ -0,0 +1,13 @@
+package br.com.cp.api_cp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCpApplication.class, args);
+	}
+
+}
---
+++ api_cp/src/test/java/br/com/cp/api_cp/ApiCpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cp.api_cp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
